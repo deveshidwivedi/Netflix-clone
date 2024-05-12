0635db25dca53404c1f52da28365eaeca0b63e56
Deveshi Dwivedi <120312681+deveshidwivedi@users.noreply.github.com>
2024-05-12 15:48:57 +0530

    init

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCTtsImDahdMlvPkpLfJzs3TkaPj5JrUk0",
+  authDomain: "neflix-clone-4f739.firebaseapp.com",
+  projectId: "neflix-clone-4f739",
+  storageBucket: "neflix-clone-4f739.appspot.com",
+  messagingSenderId: "153067698006",
+  appId: "1:153067698006:web:9ad5ae70866c45fa60c8f7",
+  measurementId: "G-K04W2RJ7X1"
+};
